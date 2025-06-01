427d3a69dd264bcf100e5ed8853845fe468bae8c
Meezab Momin <meezabmomin07@gmail.com>
2025-06-01 13:01:42 +0000

    change the logo again into something Unique but good looking

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -11,19 +11,14 @@ export const Logo = (props: LucideProps) => (
     strokeLinejoin="round"
     {...props}
   >
-    {/* Outer box shape */}
-    <rect x="3" y="3" width="18" height="18" rx="2" ry="2" />
+    {/* Main shield/open box shape */}
+    <path d="M4 7V19c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V7" />
+    {/* Top lid/opening effect & subtle U */}
+    <path d="M4 7c0-1.1.9-2 2-2h12c1.1 0 2 .9 2 2" />
+    <path d="M8 7a4 4 0 0 1 8 0" />
     
-    {/* Stylized 'U' - an arch within the top part of the box */}
-    <path d="M8 13V8c0-2.21 1.79-4 4-4s4 1.79 4 4v5" />
-
-    {/* Small gear/cog shape in the bottom center of the box */}
-    <circle cx="12" cy="16" r="1.5" />
-    <path d="M12 14.5V13" /> 
-    <path d="M12 17.5V19" />
-    <path d="M14.121 15.379l1.061-1.06" /> {/* Adjusted spoke for cog-like feel */}
-    <path d="M8.818 17.682l-1.061-1.061" /> {/* Adjusted spoke */}
-    <path d="M15.182 17.682l-1.061-1.061" /> {/* Adjusted spoke */}
-    <path d="M9.879 15.379l-1.061-1.06" />  {/* Adjusted spoke */}
+    {/* Central "spark" or "utility" symbol */}
+    <path d="M12 12.5v3" /> {/* Vertical line of plus */}
+    <path d="M10.5 14h3" /> {/* Horizontal line of plus */}
   </svg>
 );
